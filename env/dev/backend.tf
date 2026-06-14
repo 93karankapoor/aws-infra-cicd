01b59785676bb5388b5911a6ed3f9a80805525fc
Karan kapoor <93karankapoor@gmail.com>
2026-06-14 15:17:06 +0530

    Updating backend

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -1,8 +1,14 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
   backend "s3" {
     bucket = "s3-iac-state-bucket"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
 }
-
