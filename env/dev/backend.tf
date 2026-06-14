5a9c5affe47a897552083c36ba32ab7089bd96a8
Karan kapoor <93karankapoor@gmail.com>
2026-06-14 17:40:22 +0530

    Updating backend

--- env/dev/backend.tf
+++ env/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 1.15"
 
   backend "s3" {
-    bucket = "s3-iac-state-bucket"
+    bucket = "s3-iac-state-bucket-1"
     key    = "dev/terraform.tfstate"
     region = "us-east-1"
   }
